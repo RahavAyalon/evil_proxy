54ad5589ff461f09587170e6317d6a673c5130b7
rahav-ayalon <rahav.ayalon@mail.huji.ac.il>
2023-05-27 01:26:42 +0300

    feds

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse
-import http.client
+import socket, sys, datetime, time
+from _thread import start_new_thread
 from bs4 import BeautifulSoup
 
-class ProxyHandler(BaseHTTPRequestHandler):
-    blocked_keywords = set()  # Set to store the blocked keywords
-    blocked_hosts = set()  # Set to store the blocked hostnames
+
+class Server:
+    # Constructors initializing basic architecture
+    BLOCKED_KEYWORDS_FILE_PATH = './keywords.txt'
+    BLOCKED_HOSTS_FILE_PATH = './hosts.txt'
+
+    def __init__(self):
+        self.blocked_keywords = set()
+        self.blocked_hosts = set()
+
+        self.load_blocked_keywords(self.BLOCKED_KEYWORDS_FILE_PATH)
+        self.load_blocked_hosts(self.BLOCKED_HOSTS_FILE_PATH)
 
     def load_blocked_keywords(self, file_path):
         with open(file_path, 'r') as file:
@@ -18,8 +26,6 @@ class ProxyHandler(BaseHTTPRequestHandler):
                 self.blocked_hosts.add(line.strip())
 
     def is_host_blocked(self, host):
-        if host in self.blocked_hosts:
-            print('Blocked by host\n')
         return host in self.blocked_hosts
 
     def is_content_blocked(self, content):
@@ -28,124 +34,153 @@ class ProxyHandler(BaseHTTPRequestHandler):
 
         for keyword in self.blocked_keywords:
             if keyword in text:
-                print("**")
-                # print(text)
-                print("**")
-                print('Blocked by content\n')
                 return True
         return False
 
-    def do_proxy_request(self, method, host, path, request_body=None):
+    # Helper Function to get Time Stamp
+    def getTimeStampp(self):
+        return "[" + str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')) + "]"
+
+    # Function which triggers the server
+    def start_server(self, conn=3, buffer=4096, port=8080):
         try:
-            conn = http.client.HTTPSConnection(host)
-            conn.request(method, path, body=request_body, headers=self.headers)
-            response = conn.getresponse()
-            response_body = response.read()
+            print(self.getTimeStampp() + "   \n\nStarting Server\n\n")
 
-            # Check if the response content is blocked
-            if self.is_content_blocked(response_body.decode()):
-                self.send_response(403)  # Forbidden
-                self.end_headers()
-                self.wfile.write(b'Access to this content is blocked.')
+            self.listen(conn, buffer, port)
+
+        except KeyboardInterrupt:
+            print(self.getTimeStampp() + "   Interrupting Server.")
+            time.sleep(.5)
+
+        finally:
+            print(self.getTimeStampp() + "   Stopping Server")
+            sys.exit()
+
+    # Listener for incoming connections
+    def listen(self, max_connections_num, buffer, port):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind(('', port))
+            s.listen(max_connections_num)
+            print(self.getTimeStampp() + "   Listening...")
+            print(self.getTimeStampp() + "   Initializing Sockets [ready] Binding Sockets [ready] Listening...")
+        except:
+            print(self.getTimeStampp() + "   Error: Cannot start listening...")
+            sys.exit(1)
+
+        while True:
+            # Try to accept new connections and read the connection data in another thread
+            try:
+                conn, addr = s.accept()
+                print(self.getTimeStampp() + "   Request received from: " + addr[0] + " at port: " + str(addr[1]))
+                start_new_thread(self.connection_read_request, (conn, addr, buffer))
+
+            except Exception as e:
+                print(self.getTimeStampp() + "  Error: Cannot establish connection..." + str(e))
+                sys.exit(1)
+
+
+    # Function to read request data
+    def connection_read_request(self, conn, addr, buffer):
+        # Try to split necessary info from the header
+        try:
+            request = conn.recv(buffer)
+            header = request.split(b'\n')[0]
+            requested_file = request
+            requested_file = requested_file.split(b' ')
+            url = header.split(b' ')[1]
+
+            # Stripping Port and Domain
+            hostIndex = url.find(b"://")
+            if hostIndex == -1:
+                temp = url
             else:
-                # Send the response back to the client
-                self.send_response(response.status)
-                for header, value in response.getheaders():
-                    self.send_header(header, value)
-                self.end_headers()
-                self.wfile.write(response_body)
+                temp = url[(hostIndex + 3):]
+
+            portIndex = temp.find(b":")
+
+            serverIndex = temp.find(b"/")
+            if serverIndex == -1:
+                serverIndex = len(temp)
+
+            # Use the port in header
+            port = int((temp[portIndex + 1:])[:serverIndex - portIndex - 1])
+            webserver = temp[:portIndex]
+
+            try:
+                if self.is_host_blocked(webserver.decode()):
+                        print(self.getTimeStampp() + "   Website Blacklisted")
+                        response = "HTTP/1.1 403 Forbidden\r\nContent-Length: 0\r\n\r\n"
+                        conn.sendall(response.encode())
+                        conn.close()
+                else:
+                     # If method is CONNECT (HTTPS)
+                    print(self.getTimeStampp() + "   CONNECT Request")
+                    print(self.getTimeStampp() + "   HTTPS Connection request")
+                    # Stripping requested file
+                    requested_file = requested_file[1]
+                    print("Requested File ", requested_file)
+                    self.https_proxy(webserver, port, conn, request, addr, buffer, requested_file)
+            except:
+                pass
+
         except Exception as e:
-            # Handle any errors that occurred during the request
-            self.send_response(500)  # Internal Server Error
-            self.end_headers()
-            self.wfile.write(str(e).encode())
+            print(self.getTimeStampp() + "  Error: Cannot read connection request..." + str(e))
+            return
+        
 
-    def do_GET(self):
-        parsed_url = urlparse(self.path)
-        host = parsed_url.netloc
-        path = parsed_url.path
+    def https_proxy(self, webserver, port, conn, request, addr, buffer_size, requested_file):
+        # Stripping filename
+        requested_file = requested_file.replace(b".", b"_").replace(b"http://", b"_").replace(b"/", b"")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            # If successful, send 200 code response
+            s.connect((webserver, port))
+            reply = "HTTP/1.0 200 Connection established\r\n"
+            reply += "Proxy-agent: Jarvis\r\n"
+            reply += "\r\n"
+            conn.sendall(reply.encode())
+        except socket.error as err:
+            pass
 
-        if self.is_host_blocked(host):
-            self.send_response(403)  # Forbidden
-            self.end_headers()
-            self.wfile.write(b'Access to this hostname is blocked.')
-        else:
-            # Proxy the GET request to the target server
-            self.do_proxy_request("GET", host, path)
+        conn.setblocking(0)
+        s.setblocking(0)
+        print(self.getTimeStampp() + "  HTTPS Connection Established")
 
-    def do_POST(self):
-        parsed_url = urlparse(self.path)
-        host = parsed_url.netloc
-        path = parsed_url.path
+        # Initialize a variable to store the website's content
+        website_content = b""
+        
+        while True:
+            try:
+                request = conn.recv(buffer_size)
+                s.sendall(request)
+            except socket.error as err:
+                pass
 
-        if self.is_host_blocked(host):
-            self.send_response(403)  # Forbidden
-            self.end_headers()
-            self.wfile.write(b'Access to this hostname is blocked.')
-        else:
-            # Proxy the POST request to the target server
-            content_length = int(self.headers['Content-Length'])
-            request_body = self.rfile.read(content_length)
+            try:
+                reply = s.recv(buffer_size)
+                
+                # Append the received data to the website_content variable
+                website_content += reply
+                
+                conn.sendall(reply)
+            except socket.error as e:
+                pass
 
-            self.do_proxy_request("POST", host, path, request_body)
+            # Check if the desired keywords are present in the website's content
+            for blocked_keyword in self.blocked_keywords:
+                if blocked_keyword.encode() in website_content:
+                    print(self.getTimeStampp() + "   Website Blacklisted")
+                    response = "HTTP/1.1 403 Forbidden\r\nContent-Length: 0\r\n\r\n"
+                    conn.sendall(response.encode())
+                    conn.close()
 
-    def do_PUT(self):
-        parsed_url = urlparse(self.path)
-        host = parsed_url.netloc
-        path = parsed_url.path
+            # If the response from the server is empty
+            if not reply:
+                break
 
-        if self.is_host_blocked(host):
-            self.send_response(403)  # Forbidden
-            self.end_headers()
-            self.wfile.write(b'Access to this hostname is blocked.')
-        else:
-            # Proxy the PUT request to the target server
-            content_length = int(self.headers['Content-Length'])
-            request_body = self.rfile.read(content_length)
 
-            self.do_proxy_request("PUT", host, path, request_body)
 
-    def do_DELETE(self):
-        parsed_url = urlparse(self.path)
-        host = parsed_url.netloc
-        path = parsed_url.path
-
-        if self.is_host_blocked(host):
-            self.send_response(403)  # Forbidden
-            self.end_headers()
-            self.wfile.write(b'Access to this hostname is blocked.')
-        else:
-            # Proxy the DELETE request to the target server
-            self.do_proxy_request("DELETE", host, path)
-
-    def do_HEAD(self):
-        parsed_url = urlparse(self.path)
-        host = parsed_url.netloc
-        path = parsed_url.path
-
-        if self.is_host_blocked(host):
-            self.send_response(403)  # Forbidden
-            self.end_headers()
-            self.wfile.write(b'Access to this hostname is blocked.')
-        else:
-            # Proxy the HEAD request to the target server
-            self.do_proxy_request("HEAD", host, path)
-
-def run_proxy_server():
-    host = 'localhost'
-    port = 8888
-    keywords_file_path = './keywords.txt'
-    hosts_file_path = './hosts.txt'
-
-    server = HTTPServer((host, port), ProxyHandler)
-    server.RequestHandlerClass.load_blocked_keywords(ProxyHandler, keywords_file_path)
-    server.RequestHandlerClass.load_blocked_hosts(ProxyHandler, hosts_file_path)
-    print(f'Proxy server is running on {host}:{port}')
-
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print('Proxy server stopped.')
-
-if __name__ == '__main__':
-    run_proxy_server()
+if __name__ == "__main__":
+    server = Server()
+    server.start_server()
--- test.py
+++ test.py
@@ -1,38 +1,80 @@
-import subprocess
-import time
 import threading
+import requests
+from termcolor import colored
 
-def start_proxy_server():
-    return subprocess.Popen(['python3', 'main.py'])
+from main import Server
 
-def wait_for_proxy_server():
-    # Wait for the proxy server to start
-    time.sleep(2)
+class ProxyServerTests:
+    @staticmethod
+    def test_block_stackoverflow():
+        server = Server()
+        blocked_host = "stackoverflow.com"
+        assert server.is_host_blocked(blocked_host) == True
+        print(colored("test_block_stackoverflow passed", "green"))
 
-def test_blocking_website(proxy_host, proxy_port):
-    stackoverflow_cmd = f'curl -x {proxy_host}:{proxy_port} http://stackoverflow.com'
-    stackoverflow_result = subprocess.run(stackoverflow_cmd, shell=True, capture_output=True, text=True)
+        # Additional test case to check a non-blocked host
+        non_blocked_host = "ipinfo.io"
+        assert server.is_host_blocked(non_blocked_host) == False
+        print(colored("test_block_stackoverflow (non-blocked host) passed", "green"))
 
-    assert 'Access to this content is blocked' in stackoverflow_result.stdout or 'Access to this hostname is blocked' in stackoverflow_result.stdout, 'Proxy server failed to block stackoverflow.com'
+    @staticmethod
+    def test_allow_ipinfo():
+        server = Server()
+        allowed_host = "ipinfo.io"
+        assert server.is_host_blocked(allowed_host) == False
+        print(colored("test_allow_ipinfo passed", "green"))
 
-def test_non_blocking_website(proxy_host, proxy_port):
-    ipinfo_cmd = f'curl -x {proxy_host}:{proxy_port} http://ipinfo.io'
-    ipinfo_result = subprocess.run(ipinfo_cmd, shell=True, capture_output=True, text=True)
-    assert 'Access to this content is blocked' not in ipinfo_result.stdout, 'Proxy server blocked ipinfo.io'
+        # Additional test case to check a blocked host
+        blocked_host = "stackoverflow.com"
+        assert server.is_host_blocked(blocked_host) == True
+        print(colored("test_allow_ipinfo (blocked host) passed", "green"))
 
-def test_proxy_server():
-    proxy_host = 'localhost'
-    proxy_port = 8888
+    @staticmethod
+    def test_concurrent_clients():
+        server = Server()
 
-    proxy_process = start_proxy_server()
+        def start_server():
+            server.start_server()
+            i = 0
+            while i < 10000:
+                i += 1
 
-    # Execute the tests
-    wait_for_proxy_server()
-    test_blocking_website(proxy_host, proxy_port)
-    test_non_blocking_website(proxy_host, proxy_port)
+        # Start the server in a separate thread
+        server_thread = threading.Thread(target=start_server)
+        server_thread.start()
 
-    # Wait for the proxy
-    proxy_process.wait()
+        def simulate_client_request(client_num):
+            if client_num == 1:
+                # Send request to ipinfo.io
+                response = requests.get("http://ipinfo.io")
+                assert response.status_code == 200
+                assert "ip" in response.json()
+                print("Client", client_num, "received response:", response.text)
+            else:
+                # Simulate client request to another blocked host
+                print("Client", client_num, "sending request to blocked host")
+                # ...
 
-if __name__ == '__main__':
-    test_proxy_server()
+        # Create and start three threads to simulate concurrent client requests
+        threads = []
+        for i in range(3):
+            t = threading.Thread(target=simulate_client_request, args=(i+1,))
+            threads.append(t)
+            t.start()
+
+        # Wait for all threads to finish
+        for t in threads:
+            t.join()
+
+        # Additional assertion to check if the server handled the clients concurrently
+        assert True  # Add your assertion here
+        print(colored("test_concurrent_clients passed", "green"))
+
+        # Stop the server after the tests are complete
+        server_thread.join()
+
+if __name__ == "__main__":
+    # Run the tests
+    ProxyServerTests.test_block_stackoverflow()
+    ProxyServerTests.test_allow_ipinfo()
+    ProxyServerTests.test_concurrent_clients()
